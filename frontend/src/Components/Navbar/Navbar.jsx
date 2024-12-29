5d886bcde36a1ef7202adac2682d95fedb001afa
Arslan Haider <arslanhaidermughal8@gmail.com>
2024-12-29 06:53:37 +0500

    Smooth scroll effect

--- frontend/src/Components/Navbar/Navbar.jsx
+++ frontend/src/Components/Navbar/Navbar.jsx
@@ -1,20 +1,21 @@
 import React, { useState } from 'react';
 import './Navbar.css';
 import {assets} from "../../assets/frontend_assets/assets"
+import {Link} from 'react-router-dom';
 
 const Navbar = () => {
 
-    const [menu, setMenu] = useState("menu");
+    const [menu, setMenu] = useState("");
 
 
   return (
     <div className='navbar'>
       <img className='logo' src={assets.logo} alt="" />
       <ul className='navbar-menu'>
-        <li onClick={()=>setMenu('home')} className={menu==='home' ? 'active' : ''}>Home</li>
-        <li onClick={()=>setMenu('menu')} className={menu==='menu' ? 'active' : ''}>Menu</li>
-        <li onClick={()=>setMenu('about')} className={menu==='about' ? 'active' : ''}>About</li>
-        <li onClick={()=>setMenu('contact')} className={menu==='contact' ? 'active' : ''}>Contact Us</li>
+        <Link to='/' onClick={()=>setMenu('home')} className={menu==='home' ? 'active' : ''}>Home</Link>
+        <a href='#explore-menu' onClick={()=>setMenu('menu')} className={menu==='menu' ? 'active' : ''}>Menu</a>
+        <a href='#app-download' onClick={()=>setMenu('app-download')} className={menu==='app-download' ? 'active' : ''}>Mobile App</a>
+        <a href='#footer' onClick={()=>setMenu('contact')} className={menu==='contact' ? 'active' : ''}>Contact Us</a>
       </ul>
       <div className='navbar-right'>
         <img src={assets.search_icon} alt="Search_Icon" />
