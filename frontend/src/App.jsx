3ef9c5709cec342c8f854b3e585e72fbf1fd722b
Arslan Haider <arslanhaidermughal8@gmail.com>
2024-12-29 07:51:44 +0500

    Sign uo & Login popup created

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import Navbar from "./Components/Navbar/Navbar";
 import { Route, Routes } from "react-router-dom";
 import Home from "./Pages/Home/Home";
@@ -7,18 +7,24 @@ import PlaceOrder from "./Pages/PlaceOrder/PlaceOrder";
 import Cart from "./Pages/Cart/Cart";
 import Contact from "./Pages/Contact/Contact";
 import Footer from "./Components/Footer/Footer";
+import LoginPopup from "./Components/LoginPopup/LoginPopup";
 
 const App = () => {
+
+
+  const [showLogin, setShowLogin] = useState(false);
+
   return (
     <>
+    {showLogin ? <LoginPopup setShowLogin={setShowLogin} /> : <></>}
       <div className="app">
-      <Navbar />
+      <Navbar setShowLogin={setShowLogin} />
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/cart" element={<Cart />} />
           <Route path="/about" element={<About />} />
-          <Route path="/Place-order" element={<PlaceOrder />} />
-          <Route path="/Place-order" element={<Contact />} />
+          <Route path="/mobile-app" element={<PlaceOrder />} />
+          <Route path="/contact" element={<Contact />} />
         </Routes>
       </div>
       <Footer />
--- frontend/src/Components/Navbar/Navbar.jsx
+++ frontend/src/Components/Navbar/Navbar.jsx
@@ -3,7 +3,7 @@ import './Navbar.css';
 import {assets} from "../../assets/frontend_assets/assets"
 import {Link} from 'react-router-dom';
 
-const Navbar = () => {
+const Navbar = ({setShowLogin}) => {
 
     const [menu, setMenu] = useState("");
 
@@ -25,7 +25,7 @@ const Navbar = () => {
 
             </div>
         </div>
-        <button>Sign In</button>
+        <button onClick={()=>setShowLogin(true)}>Sign In</button>
       </div>
    </div>
   )
