7595ec57780ee524e9d43b6eeb3f6051e57cdc19
Arslan Haider <arslanhaidermughal8@gmail.com>
2024-12-31 17:55:12 +0500

    Logic added for add cart, remove cart, and getting cart data

--- backend/routes/cart.route.js
+++ backend/routes/cart.route.js
@@ -1,11 +1,12 @@
 import express from 'express';
-import { addToCart, removeFromCart, getCart } from '../controllers/cart.controller';
+import { addToCart, removeFromCart, getCart } from '../controllers/cart.controller.js';
+import authMiddelware from '../middlewares/auth.js';
 
 const cartRouter = express.Router();
 
-cartRouter.post('/add', addToCart);
-cartRouter.post('/remove', removeFromCart);
-cartRouter.post('/get', getCart);
+cartRouter.post('/add', authMiddelware, addToCart);
+cartRouter.post('/remove', authMiddelware, removeFromCart);
+cartRouter.post('/get', authMiddelware, getCart);
 
 
 export default cartRouter;
